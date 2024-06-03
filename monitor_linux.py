6b951da4eaa32a49d7b9a1179e5ba5c204093992
ktw <dhqhfjem@gmail.com>
2024-06-03 17:09:37 +0900

    너무 긴 변수명 수정

--- monitor_linux.py
+++ monitor_linux.py
@@ -12,14 +12,13 @@ class NifiRestApiClient():
     def __init__(self):
 
         ###접속 정보 설정
-        self.usernames, self.passwords, self.address_ips, self.address_ports = self.read_conf('settings/setting.ini')
+        self.usernames, self.passwords, self.addr_ips, self.addr_ports = self.read_conf('settings/setting.ini')
         self.lin_token_path = 'settings/tokens/access_token' #토큰 경로
 
-        ###로그 경로
-        self.lin_sys_diag = 'logs/system-diagnostics.txt' #/system-diagnostics 결과 로그 경로
+        self.lin_sys_diag = 'logs/system-diagnostics.txt' #윈도우/system-diagnostics 결과 로그 경로
         self.lin_flow_stat = 'logs/flow_status.txt' #/flow/status 결과 로그 경로
         self.lin_ctr_clst = 'logs/controller_cluster.txt' #/controller/cluster 결과 로그 경로
-        self.lin_error_logs = 'logs/error_logs.txt' #에러 로그 경로
+        self.lin_error_logs = 'logs/error_logs.txt' #nifi 서버 에러 로그 경로
 
         self.access_token = None
         self.sys_diag_header = False
@@ -30,7 +29,7 @@ class NifiRestApiClient():
     def read_conf(self, ini_path: str) -> tuple:
         usernames = []
         passwords = []
-        address_ids = []
+        address_ips = []
         address_ports = []
         
         config = parser.ConfigParser()
@@ -39,10 +38,10 @@ class NifiRestApiClient():
         for section in config.sections():
             usernames.append(config[section]['username'])
             passwords.append(config[section]['password'])
-            address_ids.append(config[section]['ip'])
+            address_ips.append(config[section]['ip'])
             address_ports.append(config[section]['port'])  
 
-        return usernames, passwords, address_ids, address_ports
+        return usernames, passwords, address_ips, address_ports
 
     ###입력 받은 json의 중첩을 해제, 평탄화
     def modify_json(self, data: dict, key: str = '') -> dict:
@@ -142,19 +141,20 @@ class NifiRestApiClient():
         response = requests.post(f'https://{in_ip}:{in_port}/nifi-api/access/token', data=_data, verify=False) #verify=False 보안에 대한 SSL 인증서 체크를 안 함
 
         ###파일에 토큰 저장
-        with open(self.lin_token_path, 'w') as access_token:
-            access_token.write(response.text)
+        with open(self.lin_token_path, 'w') as token:
+            token.write(response.text)
             # print(self.access_token)    
     
     ###저장된 토큰 파일을 읽음
     def read_access_token(self) -> None:
-        with open(self.lin_token_path, 'r') as access_token: 
-            self.access_token = access_token.read().rstrip() #파일을 윈도우에서 리눅스로 옮기는 경우 원치않는 개행문자가 추가되는 경우가 있음(\n), 개행문자 제거
+        with open(self.lin_token_path, 'r') as token: 
+            self.access_token = token.read().rstrip() #파일을 윈도우에서 리눅스로 옮기는 경우 원치않는 개행문자가 추가되는 경우가 있음(\n), 개행문자 제거
 
     ###api 호출
-    def get_info(self, catch_id: str, catch_pwd: str, catch_ip: str, catch_port: str, sub_url: str) -> str:
+    def get_info(self, catch_id: str, catch_pwd: str, catch_ip: str, catch_port: str, sub_url: str) -> None:
 
         ###api 호출에 필요한 인자 세팅
+        # self.write_access_token(catch_id, catch_pwd, catch_ip, catch_port) #새로운 토큰을 발급 받아서
         self.read_access_token() #파일에서 토큰을 읽어옴
         _headers = { #api 호출에 필요한 인자
             'Content-Type': 'application/json',
@@ -188,53 +188,53 @@ class NifiRestApiClient():
 
         ############### 테스트 필요 #########################################################
         ###그 외 다른 이유로 호출에 실패했다면
-        except requests.exceptions.RequestException as req_e:
+        except Exception as e:
             
             ### 에러 결과 redirection
             current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S') # 시간 기록
             with open(self.lin_error_logs, 'a') as error_logs: 
-                error_logs.write(f'{current_time} error: url: {sub_url} response.code [{response.status_code}] {req_e}\n') # 형식: 2024-05-20 17:29:49 error: response.code [401]
+                error_logs.write(f'{current_time} error: url: {sub_url} response.code [{response.status_code}] {e}\n') # 형식: 2024-05-20 17:29:49 error: response.code [401]
 
             raise Exception(f'error code[{response.status_code}] error url: {sub_url}')
 
 def main(term: str, end_time: str):
     cli = NifiRestApiClient() 
-
+    
     ###입력 받은 인수 조건 검사
     try:    
         catch_term = cli.test_string(term)
         catch_end_time = cli.test_string(end_time)
     except ValueError as val_e:
         print(f'message {val_e}')
-
         sys.exit(1)
-    
+
+    ###입력 받는 인자를 주기로 api 호출   
+    def call_apis_for_ip(username, password, ip, port):
+        wrn.filterwarnings('ignore', message=f"Unverified HTTPS request is being made to host '{ip}'.*")
+        cli.get_info(username, password, ip, port, '/system-diagnostics')
+        cli.get_info(username, password, ip, port, '/controller/cluster')
+        cli.get_info(username, password, ip, port, '/flow/status')
+
     ###시작 시간 기록
     start_time_recod = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print(f'start_time: {start_time_recod}')
 
-    ###입력 받는 인자를 주기로 api 호출   
-    def call_apis_for_ip(ip, port, username, password):
-        wrn.filterwarnings('ignore', message=f"Unverified HTTPS request is being made to host '{ip}'.*")
-        cli.get_info(username, password, ip, port, f'/system-diagnostics')
-        cli.get_info(username, password, ip, port, f'/controller/cluster')
-        cli.get_info(username, password, ip, port, f'/flow/status')
-
     ###호출 비동기 처리
-    with ThreadPoolExecutor(max_workers=len(cli.address_ips)) as executor: #동시에 여러 쓰레드로 작업 진행, max_workers는 최대 쓰레드 개수
+    with ThreadPoolExecutor(max_workers=len(cli.addr_ips)) as executor:
         futures = []
+
         for _ in range(1, (catch_end_time + 1) // catch_term + 1):
-            for username, password, ip, port in zip(cli.usernames, cli.passwords, cli.address_ips, cli.address_ports):
-                futures.append(executor.submit(call_apis_for_ip, ip, port, username, password)) #call_apis_for_ip() 에 ip, port, username, password 전달
+            for username, password, ip, port in zip(cli.usernames, cli.passwords, cli.addr_ips, cli.addr_ports):
+                futures.append(executor.submit(call_apis_for_ip, username, password, ip, port))
+
             time.sleep(catch_term)  # 호출 주기 조절
 
-        for future in as_completed(futures): #futures 내의 모든 객체들이 완료되길 기다렸다가 완료되는 순서대로 반환
+        for future in as_completed(futures):
             try:
                 future.result()
             except Exception as e:
                 print(f'Generated an exception: {e}')
 
-    ###종료 시간 기록
     end_time_recod = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print(f'end_time: {end_time_recod}')
 
